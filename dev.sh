4078e961f4febe676ad787fe5c7cbcf326eae558
LimEunSeop <dmstjq12@naver.com>
2021-03-30 03:41:11 +0900

    shell 개선

--- dev.sh
+++ dev.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+docker-compose -f docker-compose.dev.yml down
 git pull
 docker-compose -f docker-compose.dev.yml --env-file ./config/.env.dev up --build
--- prod.sh
+++ prod.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+docker-compose -f docker-compose.prod.yml down
 git pull
-docker-compose -f docker-compose.prod.yml --env-file ./config/.env.prod up --build
+docker-compose -f docker-compose.prod.yml --env-file ./config/.env.prod up -d --build
